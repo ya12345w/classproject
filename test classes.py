0836b73379894e8d874bcc1c12368ac9f3cfbcc1
user <yahyaiogerek@gmail.com>
2025-01-16 09:21:31 +0300

    magic

--- test classes.py
+++ test classes.py
@@ -1,69 +1,13 @@
 import random
 import time
 
-# class Car:
-#     name = "lexus"
-#     speed = 30
-#     color = "black"
-#
-#     def info(self):
-#         return self.color
-#
-# car1 = Car()
-# car2 = Car()
-# car2.color="white"
-# print(car2.info())
-# print(car1.info())
-
-# class Cat:
-#     name="barsik"
-#     color="white"
-#     weikht=10
-
-# class SuperCar:
-#     def __init__(self,name,speed,color):
-#         self.Name=name
-#         self.Speed=speed
-#         self.Color=color
-#
-# car3 = SuperCar("tesla",100,"gray")
-# car4 = SuperCar("lada",90,"blue")
-#
-# print(car3.Name)
-# maleName = ["Alexander", "Alexander", "Alexander", "Alexander", "Alexander", "Michael", "Michael", "Michael", "Michael",
-#             "William", "William", "William", "James", "James", "name"]
-# femaleName = ["Anna", "Anna", "Anna", "Anna", "Anna", "Elizabeth", "Elizabeth", "Elizabeth", "Elizabeth", "Elizabeth",
-#               "Victoria", "Victoria", "Victoria", "Sophia", "Sophia", "name"]
-#
-# Gender = random.choice(["Male", "Female"])
-# HairColor = random.choice(["blue", "red", "brown", "black", "green", "yellow", "orange"])
-# Old = random.randint(1, 80)
-# if Gender == "Male":
-#
-# class Person:
-#     def __init__(self,name,age,fml,contacts,national):
-#         self.Name=name
-#         self.Age=age
-#         self.FML=fml
-#         self.Contacts=contacts
-#         self.National=national
-#     def __repr__(self):
-#         return self.Name
-# people = []
-#
-# while True:
-#     name,age,fml,contacts,national = map(str,input("введи свое имя возраст пол контакты и национальность через пробел:").split())
-#     person = Person(name,age,fml,contacts,national)
-#
-#     people.append(person)
-#
-#     print(*people)
 class Magic:
     def __init__(self,magicName,magicPower,magicType,magicBodyType):
         self.MagicName=magicName
         self.MagicPower=magicPower
         self.MagicType=magicType
         self.MagicName=magicName
+        self.MagicBodyType=magicBodyType
 
 
 
