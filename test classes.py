66d0d3b0cd54b650043c7fb19c643b213bb0ca4e
user <yahyaiogerek@gmail.com>
2025-01-16 10:03:04 +0300

    magicUpdate

--- test classes.py
+++ test classes.py
@@ -9,15 +9,26 @@ class Magic:
         self.MagicName=magicName
         self.MagicBodyType=magicBodyType
 
+    def magic_action(self,h1,h2):
+        h1Lst = [h1.Race,h1.Health,h1.Damage,h1.Speed]
+        h2Lst = [h2.Race,h2.Health,h2.Damage,h2.Speed]
+        if self.MagicType == 1:
+            for i in self.MagicBodyType:
+                h2Lst[i] -= self.MagicPower
+
+        elif self.MagicBodyType == 2:
+            for i in self.MagicBodyType:
+                h1Lst[i] +=  self.MagicPower
 
 
 class Heroes:
-    def __init__(self,race,health,damage,speed,magic):
+    def __init__(self,race,health,damage,speed,magic,heroMagicType):
         self.Race=race
         self.Health=health
         self.Damage=damage
         self.Speed=speed
         self.Magic=magic
+        self.HeroMagicType=heroMagicType
 
     def Show_health(self):
         print(f"у {self.Race} осталось здоровья: {self.Health} ")
@@ -34,15 +45,28 @@ class Heroes:
         if self.Health <= 0:
             return True
 
+    def magicUse(self):
+        rNumber = random.randint(1,100)
+
+        if self.Magic <= rNumber:
+            #доделать шанс активации
+
 
 heroes = [
-    Heroes("Orc",200,25,10,0),
-    Heroes("people",100,30,20,10),
-    Heroes("mage",60,20,20,50)
+    Heroes("Orc",200,25,10,0,None),
+    Heroes("people",100,30,20,10,3),
+    Heroes("mage",60,20,20,50,1)
          ]
 
+Magics = [
+    Magic("heal",40,2,[1]),
+    Magic("fire ball",40,1,[1]),
+    Magic("frozen ball",20,1,[2,3]),
+    Magic("Strength up",10,2,[1,2,3])
+         ]
 
 def Punch(h1, h2):
+
     if h2.evade():
         print(f"{h2.Race} уклонился от атаки")
     else:
